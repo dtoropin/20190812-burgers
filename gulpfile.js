a7dba604d1b0de4b9f37ac7342d06f739924a722
Denis Toropin <denis.toropin@yandex.ru>
2019-08-15 07:20:32 +0300

    add gulp + fonts + cscc structure

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+'use strict';
+// gulp 4
+
+var gulp = require('gulp'),
+  watch = require('gulp-watch'),
+  prefixer = require('gulp-autoprefixer'),
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  notify = require('gulp-notify'),
+  del = require('del'),
+	browserSync = require('browser-sync'),
+  reload = browserSync.reload;
+
+sass.compiler = require('node-sass');
+  
+var path = {
+  html: {
+    src: '*.html',
+    watch: '*.html'
+  },
+  style: {
+    src: 'scss/*.scss',
+    dest: 'css/',
+    watch: 'scss/**/*.scss'
+  }
+};
+
+var config = {
+	server: {
+		baseDir: './'
+	},
+	host: 'localhost',
+	port: 9000,
+	browser: ['chrome']
+};
+
+// server
+function webserver() {
+  return browserSync(config);
+}
+
+// html
+function html() {
+  return gulp.src(path.html.src)
+    .pipe(reload({stream: true}));
+}
+
+// style
+function style() {
+  return gulp.src(path.style.src)
+		.pipe(sourcemaps.init())
+		.pipe(sass({ outputStyle: 'compact' }).on('error', notify.onError()))
+		.pipe(prefixer())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(path.style.dest))
+		.pipe(reload({stream: true}));
+}
+
+// watch
+function devwatch() {
+  watch(path.html.watch, html);
+  watch(path.style.watch, style);
+}
+
+// clean
+function clean() {
+  return del([ 'css' ]);
+}
+
+// build
+var build = gulp.series(clean, gulp.parallel(style));
+
+// default
+gulp.task('default', gulp.series(build, gulp.parallel(webserver, devwatch)));
